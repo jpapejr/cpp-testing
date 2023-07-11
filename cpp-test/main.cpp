da355e488a4c5c44f82d650dab89a7bc964e0872
John Pape <jtpape@us.ibm.com>
2023-07-11 16:45:21 -0400

    Initial Commit

---
+++ cpp-test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-test
+//
+//  Created by John Pape on 7/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
